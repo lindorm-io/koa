5a1c84563e008d6acbd447952c32c77799bb8ef3
jonn <jonn@jonn.nu>
2020-11-04 09:29:02 +0100

    fix: update context

--- src/typing/index.ts
+++ src/typing/index.ts
@@ -1,8 +1,9 @@
-import { Context } from "koa";
+import { ParameterizedContext } from "koa";
 import { Logger } from "@lindorm-io/winston";
 import { TObject } from "@lindorm-io/core";
+import { IRouterParamContext } from "koa-router";
 
-export interface IKoaAppContext extends Context {
+export interface IKoaAppContext extends ParameterizedContext<any, IRouterParamContext<any, Record<any, unknown>>> {
   id: string;
   metrics: TObject<number>;
   logger: Logger;
