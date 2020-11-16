e8735ed4ae4bf924b1976f8f694e65f7abe3119c
jonn <jonn@jonn.nu>
2020-11-16 16:28:22 +0100

    typing: update koa app context typing

--- src/typing/index.ts
+++ src/typing/index.ts
@@ -3,19 +3,21 @@ import { Logger } from "@lindorm-io/winston";
 import { TObject } from "@lindorm-io/core";
 import { IRouterParamContext } from "koa-router";
 
-export interface IKoaAppContext extends ParameterizedContext<any, IRouterParamContext<any, Record<any, unknown>>> {
-  metadata: {
-    clientId: string;
-    clientEnvironment: string;
-    clientName: string;
-    clientPlatform: string;
-    clientVersion: string;
+export interface IKoaAppMetadata {
+  clientId: string;
+  clientEnvironment: string;
+  clientName: string;
+  clientPlatform: string;
+  clientVersion: string;
 
-    correlationId: string;
-    deviceId: string;
-    installationId: string;
-    sessionId: string;
-  };
+  correlationId: string;
+  deviceId: string;
+  installationId: string;
+  sessionId: string;
+}
+
+export interface IKoaAppContext extends ParameterizedContext<any, IRouterParamContext<any, Record<any, unknown>>> {
+  metadata: IKoaAppMetadata;
   metrics: TObject<number>;
   logger: Logger;
 }
