d3c773a32603a30f03fa3672b2ac85b5abccb973
Jonn Nilsson <jonn@jonn.nu>
2021-03-28 10:01:58 +0200

    fix: change variable name of err

--- src/middleware/error-middleware.ts
+++ src/middleware/error-middleware.ts
@@ -5,19 +5,19 @@ import { TNext } from "../typing";
 export const errorMiddleware = async (ctx: Context, next: TNext): Promise<void> => {
   try {
     await next();
-  } catch (error) {
-    ctx.status = error.statusCode || error.status || HttpStatus.ServerError.INTERNAL_SERVER_ERROR;
+  } catch (err) {
+    ctx.status = err.statusCode || err.status || HttpStatus.ServerError.INTERNAL_SERVER_ERROR;
     ctx.body = {
       error: {
-        code: error.errorCode || error.code || null,
-        data: error.publicData || error.data || null,
-        details: error.details || null,
-        message: error.message,
-        name: error.name || null,
-        title: error.title || null,
+        code: err.errorCode || err.code || null,
+        data: err.publicData || err.data || null,
+        details: err.details || null,
+        message: err.message,
+        name: err.name || null,
+        title: err.title || null,
       },
     };
 
-    ctx.app.emit("error", error);
+    ctx.app.emit("error", err);
   }
 };
