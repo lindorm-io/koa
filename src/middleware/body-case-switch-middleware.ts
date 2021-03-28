c8c4cf27bdcbecd1675a2325a2812f50a7919ae3
Jonn Nilsson <jonn@jonn.nu>
2021-03-28 09:31:31 +0200

    fix: only case-switch response body when it exists

--- src/middleware/body-case-switch-middleware.ts
+++ src/middleware/body-case-switch-middleware.ts
@@ -2,9 +2,9 @@ import { IKoaAppContext, TNext } from "../typing";
 import { camelKeys, snakeKeys } from "@lindorm-io/core";
 
 export const bodyCaseSwitchMiddleware = async (ctx: IKoaAppContext, next: TNext): Promise<void> => {
-  ctx.request.body = camelKeys(ctx.request.body || {});
+  ctx.request.body = camelKeys(ctx.request.body) || {};
 
   await next();
 
-  ctx.body = snakeKeys(ctx.body || {});
+  ctx.body = snakeKeys(ctx.body);
 };
