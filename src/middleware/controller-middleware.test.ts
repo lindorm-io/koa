2000713b3d24eab1fba7ecbf756d098a3ca3d219
Jonn Nilsson <jonn@jonn.nu>
2021-04-18 09:31:29 +0200

    test: update tests

--- src/middleware/controller-middleware.test.ts
+++ src/middleware/controller-middleware.test.ts
@@ -17,17 +17,17 @@ describe("controllerMiddleware", () => {
 
   test("should set controller on context", async () => {
     await expect(controllerMiddleware(Test)(ctx, next)).resolves.toBe(undefined);
-    expect(ctx.controller.test).toMatchSnapshot();
+    expect(ctx.controller.test).toStrictEqual(expect.any(Test));
   });
 
   test("should set and keep references", async () => {
     await expect(controllerMiddleware(Test)(ctx, next)).resolves.toBe(undefined);
     ctx.handler.data = "new-data";
-    expect(ctx.controller.test).toMatchSnapshot();
+    expect(ctx.controller.test).toStrictEqual(expect.any(Test));
   });
 
   test("should set controller with specific key", async () => {
     await expect(controllerMiddleware(Test, { key: "otherKey" })(ctx, next)).resolves.toBe(undefined);
-    expect(ctx.controller.otherKey).toMatchSnapshot();
+    expect(ctx.controller.otherKey).toStrictEqual(expect.any(Test));
   });
 });
--- src/middleware/handler-middleware.test.ts
+++ src/middleware/handler-middleware.test.ts
@@ -17,17 +17,17 @@ describe("controllerMiddleware", () => {
 
   test("should set handler on context", async () => {
     await expect(handlerMiddleware(Test)(ctx, next)).resolves.toBe(undefined);
-    expect(ctx.handler.test).toMatchSnapshot();
+    expect(ctx.handler.test).toStrictEqual(expect.any(Test));
   });
 
   test("should set and keep references", async () => {
     await expect(handlerMiddleware(Test)(ctx, next)).resolves.toBe(undefined);
     ctx.controller.data = "new-data";
-    expect(ctx.handler.test).toMatchSnapshot();
+    expect(ctx.handler.test).toStrictEqual(expect.any(Test));
   });
 
   test("should set handler with specific key", async () => {
     await expect(handlerMiddleware(Test, { key: "otherKey" })(ctx, next)).resolves.toBe(undefined);
-    expect(ctx.handler.otherKey).toMatchSnapshot();
+    expect(ctx.handler.otherKey).toStrictEqual(expect.any(Test));
   });
 });
