2fadd1e157a7f4f2389a2955d7d291fd8edc49c2
Jonn Nilsson <jonn@jonn.nu>
2021-06-05 14:48:26 +0200

    fix: add generic for context aware

--- src/class/KoaContextAware.ts
+++ src/class/KoaContextAware.ts
@@ -1,9 +1,12 @@
 import type { KoaContext } from "../typing";
+import { Logger } from "@lindorm-io/winston";
 
-export abstract class KoaContextAware {
-  protected readonly ctx: KoaContext;
+export abstract class KoaContextAware<Context extends KoaContext> {
+  protected readonly ctx: Context;
+  protected readonly logger: Logger;
 
-  public constructor(ctx: KoaContext) {
+  public constructor(ctx: Context) {
     this.ctx = ctx;
+    this.logger = ctx.logger.createChildLogger(this.constructor.name || "KoaContextAware");
   }
 }
